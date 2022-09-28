e63a855bcf547f102c20183edc41f86cd7c7d7ee
Dynesshely <catrol@qq.com>
2022-09-28 23:22:36 +0800

    💾 📝 ✔️ Feat, Chore, Test: 文件替换器项目创建完工测试

---
+++ Common.Update.Replacer.Test/Program.Test.cs
@@ -0,0 +1,15 @@
+using Common.Update.Replacer;
+using System.Diagnostics;
+
+namespace Common.Update.Replacer.Test
+{
+    [TestClass]
+    public class Program_Test
+    {
+        [TestMethod]
+        public void ≤‚ ‘∆Ù∂Ø≤Œ ˝()
+        {
+            Process.Start("./Common.Update.Replacer.exe", "--test-arg");
+        }
+    }
+}
